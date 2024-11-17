d3df90860d2c09011b6ea83e083a8d9a4d7f104f
Maroun Touma <touma@us.ibm.com>
2024-11-17 13:35:59 -0500

    use module name to invoke entry point
    Signed-off-by: Maroun Touma <touma@us.ibm.com>

--- transforms/language/html2parquet/kfp_ray/html2parquet_wf.py
+++ transforms/language/html2parquet/kfp_ray/html2parquet_wf.py
@@ -20,7 +20,7 @@ from workflow_support.compile_utils import ONE_HOUR_SEC, ONE_WEEK_SEC, Component
 task_image = "quay.io/dataprep1/data-prep-kit/html2parquet-ray:latest"
 
 # the name of the job script
-EXEC_SCRIPT_NAME: str = "html2parquet_transform_ray.py"
+EXEC_SCRIPT_NAME: str = "-m dpk_html2parquet.ray.transform"
 
 # components
 base_kfp_image = "quay.io/dataprep1/data-prep-kit/kfp-data-processing:latest"
