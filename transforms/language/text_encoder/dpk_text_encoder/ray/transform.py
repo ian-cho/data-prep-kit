3d7cb023e972cd2d13608664ab430f7a6482d8a6
Maroun Touma <touma@us.ibm.com>
2024-11-24 20:43:38 -0500

    added runtime notebooks
    Signed-off-by: Maroun Touma <touma@us.ibm.com>

--- transforms/language/text_encoder/dpk_text_encoder/ray/transform.py
+++ transforms/language/text_encoder/dpk_text_encoder/ray/transform.py
@@ -10,16 +10,9 @@
 # limitations under the License.
 ################################################################################
 
-import time
-from argparse import ArgumentParser, Namespace
-from typing import Any
+import sys
 
-import pyarrow as pa
-from data_processing.runtime.pure_python.runtime_configuration import (
-    PythonTransformRuntimeConfiguration,
-)
-from data_processing.transform import AbstractTableTransform, TransformConfiguration
-from data_processing.utils import CLIArgumentProvider, get_logger
+from data_processing.utils import ParamsUtils, get_logger
 from data_processing_ray.runtime.ray import RayTransformLauncher
 from data_processing_ray.runtime.ray.runtime_configuration import (
     RayTransformRuntimeConfiguration,
@@ -43,6 +36,38 @@ class TextEncoderRayTransformConfiguration(RayTransformRuntimeConfiguration):
         :param base_configuration - base configuration class
         """
         super().__init__(transform_config=TextEncoderTransformConfiguration())
+
+
+#Class used by the notebooks to ingest binary files and create parquet files
+class TextEncoderRuntime():
+    def __init__(self, **kwargs):
+        self.params={}
+        for key in kwargs:
+            self.params[key]=kwargs[key]
+        # if input_folder and output_folder are specified, then assume it is represent data_local_config
+        try:
+            local_conf={k:self.params[k] for k in ('input_folder', 'output_folder')}
+            self.params['data_local_config']= ParamsUtils.convert_to_ast(local_conf)
+            del self.params['input_folder']
+            del self.params['output_folder']
+        except:
+            pass        
+        try:
+            worker_options={k:self.params[k] for k in ('num_cpus', 'memory')}
+            self.params['runtime_worker_options']= ParamsUtils.convert_to_ast(worker_options)
+            del self.params['num_cpus']
+            del self.params['memory']
+        except:
+            pass
+
+    
+    def transform(self):
+        sys.argv = ParamsUtils.dict_to_req(d=(self.params))
+        # create launcher
+        launcher = RayTransformLauncher(TextEncoderRayTransformConfiguration())
+        # launch
+        return_code = launcher.launch()
+        return return_code
 
 
 if __name__ == "__main__":
