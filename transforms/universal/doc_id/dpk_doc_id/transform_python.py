3b2420bdc7466081cb2bffad8d15e308c3e83a22
Maroun Touma <touma@us.ibm.com>
2024-12-10 22:29:33 +0100

    fix notebook
    Signed-off-by: Maroun Touma <touma@us.ibm.com>

--- transforms/universal/doc_id/dpk_doc_id/transform_python.py
+++ transforms/universal/doc_id/dpk_doc_id/transform_python.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-
+import sys
 from argparse import Namespace
 from typing import Any
 
@@ -20,6 +20,7 @@ from data_processing.runtime.pure_python import (
     PythonTransformRuntimeConfiguration,
 )
 from data_processing.transform import TransformStatistics
+from data_processing.utils import ParamsUtils
 from dpk_doc_id.transform import (
     DocIDTransformBase,
     DocIDTransformConfigurationBase,
@@ -112,6 +113,29 @@ class DocIDPythonTransformRuntimeConfiguration(PythonTransformRuntimeConfigurati
             transform_config=DocIDTransformConfiguration(),
             runtime_class=DocIDPythonRuntime,
         )
+
+
+class DocIDRuntime:
+    def __init__(self, **kwargs):
+        self.params = {}
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+        # if input_folder and output_folder are specified, then assume it is represent data_local_config
+        try:
+            local_conf = {k: self.params[k] for k in ("input_folder", "output_folder")}
+            self.params["data_local_config"] = ParamsUtils.convert_to_ast(local_conf)
+            del self.params["input_folder"]
+            del self.params["output_folder"]
+        except:
+            pass
+
+    def transform(self):
+        sys.argv = ParamsUtils.dict_to_req(d=(self.params))
+        # create launcher
+        launcher = PythonTransformLauncher(DocIDPythonTransformRuntimeConfiguration())
+        # launch
+        return_code = launcher.launch()
+        return return_code
 
 
 if __name__ == "__main__":
