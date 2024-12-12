bb928818fa4772e2483fc7ee119cbc480aada6f9
Maroun Touma <touma@us.ibm.com>
2024-12-12 11:37:25 +0100

    added notebooks
    Signed-off-by: Maroun Touma <touma@us.ibm.com>

--- transforms/universal/ededup/dpk_ededup/transform_python.py
+++ transforms/universal/ededup/dpk_ededup/transform_python.py
@@ -9,6 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+import sys
 from argparse import Namespace
 from typing import Any
 
@@ -19,6 +20,7 @@ from data_processing.runtime.pure_python import (
     PythonTransformRuntimeConfiguration,
 )
 from data_processing.transform import TransformStatistics
+from data_processing.utils import ParamsUtils
 from dpk_ededup.transform_base import (
     EdedupTransformBase,
     EdedupTransformConfigurationBase,
@@ -138,6 +140,30 @@ class EdedupPythonTransformRuntimeConfiguration(PythonTransformRuntimeConfigurat
             transform_config=EdedupTransformConfiguration(),
             runtime_class=EdedupRuntime,
         )
+
+
+# Class used by the notebooks to ingest binary files and create parquet files
+class Ededup:
+    def __init__(self, **kwargs):
+        self.params = {}
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+        # if input_folder and output_folder are specified, then assume it is represent data_local_config
+        try:
+            local_conf = {k: self.params[k] for k in ("input_folder", "output_folder")}
+            self.params["data_local_config"] = ParamsUtils.convert_to_ast(local_conf)
+            del self.params["input_folder"]
+            del self.params["output_folder"]
+        except:
+            pass
+
+    def transform(self):
+        sys.argv = ParamsUtils.dict_to_req(d=(self.params))
+        # create launcher
+        launcher = PythonTransformLauncher(EdedupPythonTransformRuntimeConfiguration())
+        # launch
+        return_code = launcher.launch()
+        return return_code
 
 
 if __name__ == "__main__":
